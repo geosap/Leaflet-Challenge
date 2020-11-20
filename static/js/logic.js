9a6b0f30b29e39e8a9c0842d22be68abc4ee32ec
George Sapp <geosap@george.sapp@childrens.com>
2020-11-19 22:22:22 -0600

    garth

--- static/js/logic.js
+++ static/js/logic.js
@@ -26,27 +26,75 @@ d3.json(url, jsonData => {
 
         L.circle([lat, lng], {
             color: 'black',
+            weight: .3,
             fillColor: getColor(depth),
             fillOpacity: 1,
             radius: mag * 25000
           }).addTo(myMap);
+
+          var info = L.control();
+
+          info.onAdd = function (myMap) {
+              this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+              this.update();
+              return this._div;
+          };
+          
+          // method that we will use to update the control based on feature properties passed
+          info.update = function (obj) {
+              this._div.innerHTML = '<h4>lat</h4>' +  (obj ?
+                  '<b>' + lat + '</b><br />' + lat + ' people / mi<sup>2</sup>'
+                  : 'Hover over a state');
+          };
+          
+          info.addTo(myMap);
+          
+
+
+
+
+
     });
     // Create a circle and pass in some initial options
 })
 
+
 function getColor(depth) {
-    switch (true) {
-        case depth > 60:
-            return 'red';
-        case depth > 30:
-            return 'yellow';
-        case depth < 31:
-            return 'green';
-    
-        default:
-            break;
-    }
+    return depth > 140 ? '#800026' :
+           depth > 120  ? '#BD0026' :
+           depth > 100  ? '#E31A1C' :
+           depth > 80  ? '#FC4E2A' :
+           depth > 60   ? '#FD8D3C' :
+           depth > 40   ? '#FEB24C' :
+           depth > 20   ? '#FED976' :
+                      '#FFEDA0';
 }
+
+// var info = L.control();
+
+// info.onAdd = function (myMap) {
+//     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+//     this.update();
+//     return this._div;
+// };
+
+// // method that we will use to update the control based on feature properties passed
+// info.update = function (props) {
+//     this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+//         '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+//         : 'Hover over a state');
+// };
+
+// info.addTo(myMap);
+
+
+
+
+
+
+
+
+
 
   // Set up the legend
   var legend = L.control({position: 'bottomright'});
@@ -54,17 +102,17 @@ function getColor(depth) {
   legend.onAdd = function (myMap) {
   
       var div = L.DomUtil.create('div', 'info legend'),
-          grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+          mag = [0, 20, 40, 60, 80, 100, 120, 140],
           labels = [];
   
       // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < grades.length; i++) {
+      for (var i = 0; i < mag.length; i++) {
           div.innerHTML +=
-              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+              '<i style="background:' + getColor(mag[i] + 1) + '"></i> ' +
+              mag[i] + (mag[i + 1] ? '&ndash;' + mag[i + 1] + '<br>' : '+');
       }
   
       return div;
   };
   
-  legend.addTo(map);
+  legend.addTo(myMap);
